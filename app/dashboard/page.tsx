35fd2b1527bb430c53454be67805449522195c77
Sneaxiii <misterbalise2@gmail.com>
2025-06-15 13:13:51 +0200

    Affichage des user en cours

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,13 @@
 "use client";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import React, { useEffect, useState } from "react";
 
 interface User {
@@ -19,22 +28,23 @@ const UserCard: React.FC<User> = ({
   created_at,
   last_login_date,
   disabled,
+  deleted,
 }) => {
   return (
-    <li
-      key={id}
-      className="p-4 bg-gray-100 rounded-xl shadow-md hover:bg-gray-200 transition"
-    >
-      <p className="font-semibold">Login: {login}</p>
-      <p>Email: {email}</p>
-      <p>Role: {role}</p>
-      <p>Created At: {new Date(created_at).toLocaleString()}</p>
-      <p>
-        Last Login:
-        {last_login_date ? new Date(last_login_date).toLocaleString() : "Never"}
-      </p>
-      <p>Status: {disabled ? "Disabled" : "Active"}</p>
-    </li>
+    <TableRow>
+      <TableCell className="font-semibold">{login}</TableCell>
+      <TableCell>{email}</TableCell>
+      <TableCell>{role}</TableCell>
+      <TableCell>{new Date(created_at).toLocaleString()}</TableCell>
+      <TableCell>
+        {last_login_date
+          ? new Date(last_login_date).toLocaleString()
+          : "Jamais"}
+      </TableCell>
+      <TableCell>
+        {deleted ? "Supprimé" : disabled ? "Désactivé" : "Activé"}
+      </TableCell>
+    </TableRow>
   );
 };
 const UserList: React.FC = () => {
@@ -83,14 +93,30 @@ const UserList: React.FC = () => {
 
   return (
     <div className="p-4">
+      {/* <Pagination /> */}
       <h1 className="text-2xl font-bold mb-4">User List</h1>
       <ul className="space-y-2">
         <div className="total_pages">Nombre d'utilisateurs : {totalUsers}</div>
         <div className="total_pages">Nombre de pages : {totalPages}</div>
         <div className="current_page">Page actuelle : {currentPage}</div>
-        {users.map((user) => (
-          <UserCard key={user.id} {...user} />
-        ))}
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Pseudonyme</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Role</TableHead>
+              <TableHead>Création</TableHead>
+              <TableHead>Derniere connexion</TableHead>
+              <TableHead>Etat</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users.map((user) => (
+              <UserCard key={user.id} {...user} />
+            ))}
+          </TableBody>
+        </Table>
       </ul>
     </div>
   );
