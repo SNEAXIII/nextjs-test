de30f01c2e2815043c542d4fd08f30e968aa6054
Sneaxiii <misterbalise2@gmail.com>
2025-06-25 20:24:11 +0200

    sus

--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,75 @@
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { loginForAccessToken } from '@/app/services/users';
+import { parseJwt } from '@/app/lib/utils';
+
+interface UserPayload {
+  login: string;
+  email: string;
+  role: string;
+  exp: number;
+}
 
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  providers: [],
+  providers: [
+    Credentials({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        login: { label: 'Login', type: 'text', placeholder: "Saisissez votre nom d'utilisateur" },
+        password: { label: 'Password', type: 'password', placeholder: 'Votre mot de passe' },
+      },
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ login: z.string(), password: z.string() })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) {
+          console.log('Invalid credentials format');
+          return null;
+        }
+
+        const { login: userLogin, password } = parsedCredentials.data;
+
+        try {
+          const response = await loginForAccessToken(userLogin, password);
+          const token = response.access_token;
+
+          if (!response || !token) {
+            console.log('Failed to obtain access token');
+            return null;
+          }
+
+          const tokenPayload = parseJwt(token);
+          const user: UserPayload = {
+            login: tokenPayload.sub,
+            email: tokenPayload.email,
+            role: tokenPayload.role,
+            exp: tokenPayload.exp,
+          };
+
+          return user;
+        } catch (error) {
+          console.error('Authentication failed:', error);
+          return null;
+        }
+      },
+    }),
+  ],
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        return isLoggedIn;
-        // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user; // Attach user payload to token
       }
-      return true;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user; // Make user information available in the session
+      return session;
     },
   },
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -5,58 +5,9 @@ import { loginForAccessToken } from '@/app/services/users'; // Import the login 
 import { authConfig } from './auth.config';
 import { parseJwt } from '@/app/lib/utils';
 
-interface UserPayload {
-  login: string;
-  email: string;
-  role: string;
-  exp: number;
-}
+
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [
-    Credentials({
-      id: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        login: { label: 'Login', type: 'text', placeholder: "Saisissez votre nom d'utilisateur" },
-        password: { label: 'Password', type: 'password', placeholder: 'Votre mot de passe' },
-      },
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ login: z.string(), password: z.string() })
-          .safeParse(credentials);
-
-        if (!parsedCredentials.success) {
-          console.log('Invalid credentials format');
-          return null;
-        }
-
-        const { login: userLogin, password } = parsedCredentials.data;
-
-        try {
-          const response = await loginForAccessToken(userLogin, password);
-          const token = response.access_token;
-
-          if (!response || !token) {
-            console.log('Failed to obtain access token');
-            return null;
-          }
-
-          const tokenPayload = parseJwt(token);
-          const user: UserPayload = {
-            login: tokenPayload.sub,
-            email: tokenPayload.email,
-            role: tokenPayload.role,
-            exp: tokenPayload.exp,
-          };
-
-          return user;
-        } catch (error) {
-          console.error('Authentication failed:', error);
-          return null;
-        }
-      },
-    }),
-  ],
+  
 });
