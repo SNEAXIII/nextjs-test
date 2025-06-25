aee062ffd96d634efb54b13f46611c916ce6a723
Sneaxiii <misterbalise2@gmail.com>
2025-06-25 20:17:38 +0200

    temp commit

--- auth.config.ts
+++ auth.config.ts
@@ -4,17 +4,14 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  providers: [
-    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
-    // while this file is also used in non-Node.js environments
-  ],
+  providers: [],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return isLoggedIn;
+        // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
--- auth.ts
+++ auth.ts
@@ -7,51 +7,56 @@ import { parseJwt } from '@/app/lib/utils';
 
 interface UserPayload {
   login: string;
+  email: string;
   role: string;
   exp: number;
 }
 
 export const { auth, signIn, signOut } = NextAuth({
-    ...authConfig,
-    providers: [
-      Credentials({
-        id: 'credentials',
-        name: 'Credentials',
-        credentials: {
-          login: { label: 'Login', type: 'text', placeholder: 'Saisissez votre nom d\'utilisateur' },
-          password: { label: 'Password', type: 'password', placeholder: 'Votre mot de passe' },
-        },
-        async authorize(credentials) {
-          const parsedCredentials = z
-            .object({ login: z.string(), password: z.string() })
-            .safeParse(credentials);
+  ...authConfig,
+  providers: [
+    Credentials({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        login: { label: 'Login', type: 'text', placeholder: "Saisissez votre nom d'utilisateur" },
+        password: { label: 'Password', type: 'password', placeholder: 'Votre mot de passe' },
+      },
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ login: z.string(), password: z.string() })
+          .safeParse(credentials);
 
-          if (!parsedCredentials.success) {
-            console.log('Invalid credentials format');
+        if (!parsedCredentials.success) {
+          console.log('Invalid credentials format');
+          return null;
+        }
+
+        const { login: userLogin, password } = parsedCredentials.data;
+
+        try {
+          const response = await loginForAccessToken(userLogin, password);
+          const token = response.access_token;
+
+          if (!response || !token) {
+            console.log('Failed to obtain access token');
             return null;
           }
 
-          const { login: userLogin, password } = parsedCredentials.data;
+          const tokenPayload = parseJwt(token);
+          const user: UserPayload = {
+            login: tokenPayload.sub,
+            email: tokenPayload.email,
+            role: tokenPayload.role,
+            exp: tokenPayload.exp,
+          };
 
-          try {
-            const response = await loginForAccessToken(userLogin, password);
-            const token = response.access_token;
-
-            if (!response || !response.access_token) {
-              console.log('Failed to obtain access token');
-              return null;
-            }
-            const tokenPayload = parseJwt(token);
-            const user: UserPayload = {
-              login: tokenPayload.sub,
-              role: tokenPayload.role,
-              exp: tokenPayload.exp,
-            };
-            return user;
-
-
-
-        } catch(error) {
+          return user;
+        } catch (error) {
           console.error('Authentication failed:', error);
           return null;
         }
+      },
+    }),
+  ],
+});
